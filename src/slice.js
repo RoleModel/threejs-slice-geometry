3123c0397e88f46a53a792a0aeb927f4ba55f134
John Calvin Young <john.young@rolemodelsoftware.com>
2019-09-25 12:29:33 -0400

    Bring in alternate contour-finding code and earcut to fill edge loops

--- src/slice.js
+++ src/slice.js
@@ -1,5 +1,6 @@
-var facesFromEdges = require('./faces-from-edges.js');
+//var facesFromEdges = require('./faces-from-edges.js');
 
+var earcut = require('earcut')
 
 module.exports = function(THREE) {
     "use strict";
@@ -9,6 +10,99 @@ module.exports = function(THREE) {
     var ON = 'on';
 
     var FACE_KEYS = ['a', 'b', 'c'];
+
+    THREE.Vector3.prototype.equals = function(v, tolerance) {
+      if (tolerance === undefined) {
+        return ((v.x === this.x) && (v.y === this.y) && (v.z === this.z));
+      } else {
+        return ((Math.abs(v.x - this.x) < tolerance) && (Math.abs(v.y - this.y) < tolerance) && (Math.abs(v.z - this.z) < tolerance));
+      }
+    }
+
+
+    // contour-finding code based on https://stackoverflow.com/a/46811485/2100919
+    var getContours = function(points, contours, firstRun) {
+      //console.log("firstRun:", firstRun);
+
+      let contour = [];
+
+      // find first line for the contour
+      let firstPointIndex = 0;
+      let secondPointIndex = 0;
+      let firstPoint, secondPoint;
+      for (let i = 0; i < points.length; i++) {
+        if (points[i].checked == true) continue;
+        firstPointIndex = i;
+        firstPoint = points[firstPointIndex];
+        firstPoint.checked = true;
+        secondPointIndex = getPairIndex(firstPoint, firstPointIndex, points);
+        secondPoint = points[secondPointIndex];
+        secondPoint.checked = true;
+        contour.push(firstPoint.clone());
+        contour.push(secondPoint.clone());
+        break;
+      }
+
+      contour = getContour(secondPoint, points, contour);
+      contours.push(contour);
+      let allChecked = 0;
+      points.forEach(p => { allChecked += p.checked == true ? 1 : 0; });
+      //console.log("allChecked: ", allChecked == points.length);
+      if (allChecked != points.length) { return getContours(points, contours, false); }
+      return contours;
+    }
+
+    var getContour = function(currentPoint, points, contour) {
+      let p1Index = getNearestPointIndex(currentPoint, points);
+      let p1 = points[p1Index];
+      p1.checked = true;
+      let p2Index = getPairIndex(p1, p1Index, points);
+      let p2 = points[p2Index];
+      p2.checked = true;
+      let isClosed = p2.equals(contour[0], 1e-6);
+      if (!isClosed) {
+        if(p1Index === 0) {
+          console.error('DEGENERATE CASE')
+          return contour
+        }
+        contour.push(p2.clone());
+        return getContour(p2, points, contour);
+      } else {
+        contour.push(contour[0].clone());
+        return contour;
+      }
+    }
+
+    var getPairIndex = function(point, points){
+      let index = 0;
+      for (let i = 0; i < points.length; i++){
+        let p = points[i];
+        if (p.checked !== true && p.equals(point, 1e-6)){
+          index = i;
+          break;
+        }
+      }
+      return index;
+    }
+
+    var getPairIndex = function(point, pointIndex, points) {
+      if (pointIndex % 2 === 0) {
+        return pointIndex + 1
+      } else {
+        return pointIndex - 1
+      }
+
+      // let index = 0;
+      // for (let i = 0; i < points.length; i++) {
+      //   let p = points[i];
+      //   if (i != pointIndex && p.checked == false && p.faceIndex == point.faceIndex) {
+      //     index = i;
+      //     break;
+      //   }
+      // }
+      // return index;
+    }
+    // end contour-finding code
 
     var sliceGeometry = function(geometry, plane, closeHoles) {
         var sliced = new THREE.Geometry();
@@ -48,7 +142,7 @@ module.exports = function(THREE) {
                 var index = face[key];
                 var distance = distances[index];
                 var position = positions[index];
-                
+
                 if (position === FRONT) {
                     if (lastPosition === BACK) {
                         builder.addIntersection(lastKey, key, lastDistance, distance);
@@ -123,19 +217,42 @@ module.exports = function(THREE) {
     };
 
     GeometryBuilder.prototype.closeHoles = function() {
-        if ( ! this.newEdges[0].length) {
-            return;
+      if ( ! this.newEdges[0].length) {
+          return;
+      }
+
+      const newVertices = this.newEdges.flat().map(index => this.targetGeometry.vertices[index].clone())
+      const contours = getContours(newVertices, [], true)
+
+      contours.forEach(contour => {
+        const contourVerts = contour.flatMap(v => v.toArray())
+        const result = earcut(contourVerts, null, 3)
+        let vertexIndex = this.targetGeometry.vertices.length // next index
+        for(let i = 0; i < (result.length / 3); i++) {
+          const offset = i*3
+          const a = contour[result[offset]].clone()
+          const b = contour[result[offset+1]].clone()
+          const c = contour[result[offset+2]].clone()
+          this.targetGeometry.vertices.push(a, b, c)
+          this.targetGeometry.faces.push(
+            new THREE.Face3(vertexIndex, vertexIndex + 1, vertexIndex + 2)
+          )
+          vertexIndex = vertexIndex + 3
         }
-        facesFromEdges(this.newEdges)
-            .forEach(function(faceIndices) {
-                var normal = this.faceNormal(faceIndices);
-                if (normal.dot(this.slicePlane.normal) > .5) {
-                    faceIndices.reverse();
-                }
-                this.startFace();
-                this.faceIndices = faceIndices;
-                this.endFace();
-            }, this);
+      })
+
+      this.targetGeometry.computeFaceNormals()
+      return
+      // facesFromEdges(this.newEdges)
+      //     .forEach(function(faceIndices) {
+      //         var normal = this.faceNormal(faceIndices);
+      //         if (normal.dot(this.slicePlane.normal) > .5) {
+      //             faceIndices.reverse();
+      //         }
+      //         this.startFace();
+      //         this.faceIndices = faceIndices;
+      //         this.endFace();
+      //     }, this);
     };
 
     GeometryBuilder.prototype.addVertex = function(key) {
